799f3e9dd1a017ca9f8a5b00ba23f53f97b85fd1
igorshamaev <sio@yandex.ru>
2024-08-29 16:53:25 +0300

    Add files via upload

--- Homework1.py
+++ Homework1.py
@@ -4,4 +4,3 @@ sum = num1 + num2
 print(sum)
 result = sum + num1 * num2
 print(result)
-print(result)
---
+++ Homework2.py
@@ -0,0 +1,8 @@
+name = 'Igor'
+age = 40
+is_student = True
+print('Name: ' + name)
+print('Age: ', age)
+age = age + 1
+print('New Age: ', age)
+print ('Is Student: ', is_student)

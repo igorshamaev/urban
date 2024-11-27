1c1dee0a21e2622a837e25c505ab5f9fb9ded729
igorshamaev <sio@yandex.ru>
2024-11-27 18:27:51 +0300

    Add files via upload

--- Selenium4.py
+++ Selenium4.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait as Wait
 from selenium.webdriver.support import expected_conditions as ec
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
 ch_options = Options()
 ch_options.add_experimental_option("excludeSwitches", ["enable-logging"])
@@ -24,7 +24,7 @@ goods1 = 'Суккулент Сервер'
 goods2 = 'Кактус Кодинга'
 goods3 = 'Эко-Пиксель'
 
-time.sleep(3)
+time.sleep(8)
 click_fv = driver.find_element(By.XPATH, '//*[@id="rec634117022"]/div/div[1]/div[1]/a/div/div[1]/a')
 click_fv.click() # FV1 click
 
@@ -38,33 +38,45 @@ click_fv.click() # FV3 click
 
 time.sleep(2)
 click_fv = driver.find_element(By.CLASS_NAME, 't1002__wishlisticon-imgwrap')
-click_fv.click() # FV_list click
+click_fv.click() # FV_list_open click
 
 print('-----------------------------------------------------')
 try:
-    bubble = Wait(driver, 2).until(ec.presence_of_element_located((By.XPATH, '//*[@id="rec634117498"]/div/div[2]/div/div[3]/div[1]')))
+    added_fv = Wait(driver, 2).until(ec.presence_of_element_located((By.XPATH, '//*[@id="rec634117498"]/div/div[2]/div/div[3]/div[1]')))
     print(f"Товар1 '{goods1}' добавлен в Избранное.")
     print(Fore.GREEN + f"Тест1 пройден." + Style.RESET_ALL)
 except TimeoutException:
     print(Fore.YELLOW + f"Ошибка: Товар1 '{goods1}' не добавлен в Избранное!")
-    print(Fore.RED + f"Тест1 не пройден." + Style.RESET_ALL)
+    print(Fore.RED + f"Тест1.1 не пройден." + Style.RESET_ALL)
 print('-----------------------------------------------------')
 try:
-    bubble = Wait(driver, 2).until(ec.presence_of_element_located((By.XPATH, '//*[@id="rec634117498"]/div/div[2]/div/div[3]/div[2]')))
+    added_fv = Wait(driver, 2).until(ec.presence_of_element_located((By.XPATH, '//*[@id="rec634117498"]/div/div[2]/div/div[3]/div[2]')))
     print(f"Товар2 '{goods2}' добавлен в Избранное.")
     print(Fore.GREEN + f"Тест2 пройден." + Style.RESET_ALL)
 except TimeoutException:
     print(Fore.YELLOW + f"Ошибка: Товар2 '{goods2}' не добавлен в Избранное!")
-    print(Fore.RED + f"Тест2 не пройден." + Style.RESET_ALL)
+    print(Fore.RED + f"Тест2.1 не пройден." + Style.RESET_ALL)
 print('-----------------------------------------------------')
 try:
-    bubble = Wait(driver, 2).until(ec.presence_of_element_located((By.XPATH, '//*[@id="rec634117498"]/div/div[2]/div/div[3]/div[3]')))
+    added_fv = Wait(driver, 2).until(ec.presence_of_element_located((By.XPATH, '//*[@id="rec634117498"]/div/div[2]/div/div[3]/div[3]')))
     print(f"Товар3 '{goods3}' добавлен в Избранное.")
     print(Fore.GREEN + f"Тест3 пройден." + Style.RESET_ALL)
 except TimeoutException:
     print(Fore.YELLOW + f"Ошибка: Товар3 '{goods3}' не добавлен в Избранное!")
-    print(Fore.RED + f"Тест3 не пройден." + Style.RESET_ALL)
-    # Close FV
+    print(Fore.RED + f"Тест3.1 не пройден." + Style.RESET_ALL)
+print('-----------------------------------------------------')
+time.sleep(5)
+try:
+    click_fv = driver.find_element(By.XPATH, '//*[@id="rec634117498"]/div/div[2]/div/div[3]/div[1]/div[4]/img')
+    click_fv.click()  # Remove FV1 click
+    time.sleep(5)
+    driver.find_element(By.XPATH, '//*[@id="rec634117498"]/div/div[2]/div/div[3]/div[1]')
+    print(Fore.YELLOW + f"Ошибка: Товар1 '{goods1}' не удалён из Избранного!")
+    print(Fore.RED + f"Тест1.2 не пройден." + Style.RESET_ALL)
+except NoSuchElementException:
+    print(f"Товар1 '{goods1}' удалён из Избранного.")
+    print(Fore.GREEN + f"Тест1.2 пройден." + Style.RESET_ALL)
+
     click_fv = driver.find_element(By.XPATH, '//*[@id="rec634117498"]/div/div[2]/div/div[1]/button')
     click_fv.click() # Close_FV_list click
 
